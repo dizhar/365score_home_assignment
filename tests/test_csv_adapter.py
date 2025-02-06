c544da3d0502688956f144a445b53a11394be8a0
Daniel Izhar <danielizhar@Tanyas-MacBook-Air.local>
2025-02-06 17:52:12 +0200

    modify test_csv_adapter test

--- tests/test_csv_adapter.py
+++ tests/test_csv_adapter.py
@@ -15,11 +15,15 @@
 import allure
 import pytest
 from utils.csv_adapter import CSVAdapter
+from utils.api_client import APIClient
+
 
 @allure.feature("Data-Driven Testing")
 @allure.story("Verify API with multiple test data entries")
 @pytest.mark.parametrize("test_data", CSVAdapter("test_data.csv").read_data())
-def test_post_creation(test_data, api_client):
+def test_post_creation(test_data):
+    api = APIClient()
+    
     """Test creating posts using data from CSV"""
     user_id = int(test_data["user_id"])
     title = test_data["title"]
@@ -27,5 +31,8 @@ def test_post_creation(test_data, api_client):
 
     with allure.step(f"Create a post for User ID {user_id}"):
         post_data = {"userId": user_id, "title": title, "body": body}
-        post_response = api_client.post("/posts", post_data)
+        post_response = api.post("/posts", post_data)
+        response_data = post_response.json()
+        assert response_data["title"] == post_data["title"]
+        assert response_data["body"] == post_data["body"]
         assert post_response.status_code in [200, 201]
